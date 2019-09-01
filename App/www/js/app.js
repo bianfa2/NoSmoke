9bb3eeded87a31d4a86694b135bafe84a8065795
Andguja <andguja@gmail.com>
2019-09-01 02:15:57 -0500

    Tab 3 full
    Beneficios y consecuencias

--- App/www/js/app.js
+++ App/www/js/app.js
@@ -14,6 +14,30 @@ var app = new Framework7({
         {
             path: '/ligthers/',
             url: 'pages/ligthers-page.html',
+        },
+        {
+            path: '/pulmones/',
+            url: 'pages/Pulmon-page.html',
+        },
+        {
+            path: '/corazon/',
+            url: 'pages/Corazon-page.html',
+        },
+        {
+            path: '/cerebro/',
+            url: 'pages/Cerebro-page.html',
+        },
+        {
+            path: '/cabeza/',
+            url: 'pages/CabezaRostro-page.html',
+        },
+        {
+            path: '/ADN/',
+            url: 'pages/ADN-page.html',
+        },
+        {
+            path: '/abdomen/',
+            url: 'pages/Abdomen-page.html',
         }
     ],
 });
