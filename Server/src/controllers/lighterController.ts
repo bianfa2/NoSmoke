32cf071133f3e31cd2542679d3edc7e821c8bf14
jose2490 <jgabriel50@hotmail.com>
2019-09-23 21:06:07 -0500

    Get encendedores
    se busca si existe un encendedor

--- Server/src/controllers/lighterController.ts
+++ Server/src/controllers/lighterController.ts
@@ -2,33 +2,33 @@ import {Request, Response, response} from 'express';
 import db from '../database';
 
 class LigtherController{
-    public data(req: Request,res:Response) {        
+    public data(req: Request,res:Response){        
         console.log(req.body);
-        console.log('funcionamiento lighter controller')
-
         
-  res.json({text:'funcionamiento del get'})
-      //  res.json(db.collection('datosEncendedor').doc('81Y3vsbFVHhiiycMAVrS'));
-
-      //   let observer = doc.onSnapshot((docSnapshot: any) => {          
-      //     console.log(`Received doc snapshot: ${docSnapshot}`+docSnapshot.data().encendida);
-      //       res.json({datas:observer});
-      //   }, (err:any) => {
-      //     console.log(`Encountered error: ${err}`);
-      //   });
+    db.collection('encendedores').doc(req.body.id).get()
+    .then((doc:any) => {
+      if (!doc.exists) {
+       res.json({exists:false})
+      } else {
+        res.json({exists:true})
+      }
+    })
+      .catch((err: any) => {
+      console.log('Error getting document', err);
+      });
     }
 
     create(req: Request, res: Response){
-        console.log(req.body);
-        db.collection("referenciaEncendedor").doc().set(req.body)
-        .then(function(){      
-            console.log("Documento esctrito")            
-          })
-          .catch(function(error: Error){
-            console.error("Error escribiendo el documento: ",error)
-          });
+        // console.log(req.body);
+        // db.collection("referenciaEncendedor").doc().set(req.body)
+        // .then(function(){      
+        //     console.log("Documento esctrito")            
+        //   })
+        //   .catch(function(error: Error){
+        //     console.error("Error escribiendo el documento: ",error)
+        //   });
 
-        res.json({text:'creating smoking data'});
+        // res.json({text:'creating smoking data'});
     }
 }
 
